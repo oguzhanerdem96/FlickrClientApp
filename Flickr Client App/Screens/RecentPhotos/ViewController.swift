1e5ac3c51d06b74574f548cc44d2b971b07b2ecf
oguzhanerdem96 <oguzhan_erdem96@hotmail.com>
2022-10-19 17:25:03 +0300

    first commit

---
+++ Flickr Client App/Screens/RecentPhotos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flickr Client App
+//
+//  Created by Oğuzhan Erdem on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
